4cea1c06ff3e1e87db9470d3f4b12f95a84f6c07
Adedamola <adedamolagig@gmail.com>
2025-03-25 22:21:55 +0100

    all CRUD operations are active

--- server.js
+++ server.js
@@ -1,14 +1,55 @@
 
 require("dotenv").config();
 const express = require("express");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); //Middleware to parse JSON
+app.use(express.json()); /** Middleware to parse JSON */
 
-app.get("/", (req, res) => {
-	res.json({ message: "Hello, World! Your API is working"});
+/** Sample data (temporary in-memory storage);*/
+
+let users = [
+	{id: 1, name: "Julia"},
+	{id:2, name: "Jewel"}
+];
+
+//GET all users
+app.get("/users", (req, res) => {
+	res.json(users);
 });
+
+//GET a single user by ID
+app.get("/users/:id", (req, res) => {
+	const user = users.find(u => u.id === parseInt(req.params.id));
+	if(!user) return res.status(404).json({ message: "User not found"});
+	res.json(user);
+});
+
+//POST -- Add a new user
+
+app.post("/users", (req, res) => {
+	const { name } = req.body;
+	const newUser = { id: users.length +1, name};
+	users.push(newUser);
+	res.status(201).json(newUser);
+});
+
+// PUT --Update a user
+app.put("/users/:id", (req, res) => {
+	const user = users.find(u => u.id === parseInt(req.params.id));
+	if (!user) return res.status(404).json({ message: "User not found"});
+	user.name = req.body.name || user.name;
+	res.json(user);
+});
+
+//DELETE -- Remove a user
+app.delete("/users/:id", (req, res) => {
+	users = users.filter(u => u.id !== parseInt(req.params.id));
+	res.json({ message: "User deleted" });
+});
+
+
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
